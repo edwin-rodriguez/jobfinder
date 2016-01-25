f826f5c22700b51c93ee2fb580b1639b1ee8ba25
edwin-rodriguez <ing.erodriguezm@gmail.com>
2016-01-25 14:53:13 -0500

    make the push to fail

--- test/jobs-data-specs.js
+++ test/jobs-data-specs.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 
 describe('get jobs', function(){
   it('should never be empty', function (){
-    jobsList = [];
+    jobsList = [{}];
     expect(jobsList.length).to.be.at.least(1);
   });
 });
